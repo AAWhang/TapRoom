2f1f5bdcbb243bd76d9628c58dc9ff4be8d17ec6
Student <example@example.com>
2019-09-26 10:52:01 -0700

    added variavle styling for pint amount

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -5,12 +5,20 @@ import PropTypes from 'prop-types';
 
 
 function Keg(props){
+  const good = { color: 'green' };
+  const medium = { color: 'orange' };
+  const warning = { color: 'red' };
+  let pints;
+
+  if (props.pints > 80) pints = <p>pints: <span style={good}>{props.pints}</span></p>;
+  else if (props.pints > 40) pints = <p>pints: <span style={medium}>{props.pints}</span></p>;
+  else if (props.pints > 0) pints = <p>pints: <span style={warning}>{props.pints}</span></p>;
 
   return (
     <div>
       <h3>{props.brand} - {props.names}</h3>
       <p><em>${props.price} {props.alc}% alcohol</em></p>
-      <p>Pints: {props.pints}</p>
+      {pints}
       <p>id: {props.kegId}</p>
       <button onClick={() => {props.decFunc(props.kegId)}}>Serve Pint</button>
       <button onClick={() => {props.delFunc(props.kegId)}}>Delete Keg</button>
